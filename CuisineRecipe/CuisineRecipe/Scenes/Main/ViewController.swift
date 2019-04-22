3912dbc3d816bad0b5cf68d2ae8b51ba7e1c90a2
Nguyen Duc Huy <nguyen.duc.huyb@sun-asterisk.com>
2019-04-22 14:19:46 +0700

    Task#11007

---
+++ CuisineRecipe/CuisineRecipe/Scenes/Main/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CuisineRecipe
+//
+//  Created by mac on 4/22/19.
+//  Copyright © 2019 sunasterisk. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
