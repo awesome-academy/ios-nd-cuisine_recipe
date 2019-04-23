996c5775414d774391e93bdbb3c7eddacf6444dd
Nguyen Duc Huy <nguyen.duc.huyb@sun-asterisk.com>
2019-04-23 09:58:00 +0700

    11007-fix comment

--- CuisineRecipe/CuisineRecipe/Scenes/Main/ViewController.swift
+++ CuisineRecipe/CuisineRecipe/Scenes/Main/ViewController.swift
@@ -9,10 +9,8 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 }
 
